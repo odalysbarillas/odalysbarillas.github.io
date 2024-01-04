753cb3afab6ab721a65446b697a75452b01b6ad4
Odalys <odalysbarillas46@gmail.com>
2024-01-04 02:32:07 -0600

    contacto

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
 
-  correo: string = 'odalysbarillas46@gmail.com';
-  nombre: string = 'Odalys';
+  mi_correo: string = 'odalysbarillas46@gmail.com';
+  correo: string = '';
+  nombre: string = '';
+  asunto: string = '';
+  descripcion: string = '';
 
 }
